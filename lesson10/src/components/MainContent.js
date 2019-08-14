3f26c0b75075f1b4e6c3d6d0643f394e12d77477
Дмитрий Ваганов <fourstonmain@gmail.com>
2019-08-15 01:01:49 +0300

    lesson10

---
+++ lesson10/src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <p>Main content</p>
+  )
+}
+
+export default MainContent
---
+++ lesson10/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <h1>This is navbar</h1>
+  )
+}
+
+export default Navbar

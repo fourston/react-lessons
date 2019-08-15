06160f7073a52a2e03cb3c87448b4a99759b9780
Дмитрий Ваганов <fourstonmain@gmail.com>
2019-08-16 01:30:19 +0300

    todo_app init

---
+++ todo_app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListElement from './components/ListElement';
+
+function App() {
+  return (
+    <div>
+      <ListElement />
+      <ListElement />
+      <ListElement />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo_app/src/components/ListElement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ListElement() {
+  return (
+    <div>
+      <input type="checkbox"/>
+      <span className="list-element">This is list</span>
+    </div>
+  )
+}
+
+export default ListElement
